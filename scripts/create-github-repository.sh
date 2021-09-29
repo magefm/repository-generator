6e6d4d9ea31c336e038d737e20737f89a9d00c30
Rafael Kassner <kassner@gmail.com>
2021-09-29 21:36:50 +0200

    scripts/create-github-repository.sh

---
+++ scripts/create-github-repository.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eo pipefail
+
+GHTOKENJSON="$(cat github-token.json)"
+GHUSER=$(echo "$GHTOKENJSON" | jq -r '.user')
+GHTOKEN=$(echo "$GHTOKENJSON" | jq -r '.token')
+GHORG=$(echo "$GHTOKENJSON" | jq -r '.organization')
+
+source="$1"
+PACKAGESJSON=$(php scripts/generate-package-json.php "$source")
+
+for PACKAGEJSON in $(echo "$PACKAGESJSON" | jq -c '.[]'); do
+    NAME=$(echo "$PACKAGEJSON" | jq -r '.name')
+    NAME="${NAME#magento/}"
+
+    echo "Creating ${GHORG}/${NAME}"
+
+    RESULT=$(curl -sS \
+        -u "${GHUSER}:${GHTOKEN}" \
+        -X POST \
+        -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/orgs/${GHORG}/repos" \
+        -d "{\"name\":\"${NAME}\",\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false}"
+    )
+done

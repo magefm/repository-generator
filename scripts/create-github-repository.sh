4093baec9b7c3d6eea15fba8d049be64e02f3ed9
Rafael Kassner <kassner@gmail.com>
2021-10-06 10:15:38 +0200

    scripts/create-github-repository.sh: create repositories based on available refs

--- scripts/create-github-repository.sh
+++ scripts/create-github-repository.sh
@@ -12,13 +12,28 @@ PACKAGES=($(find "$source/magento" -mindepth 1 -maxdepth 1 -type d))
 
 for PACKAGE in "${PACKAGES[@]}"; do
     NAME="${PACKAGE##*/}"
-    echo "Creating ${GHORG}/${NAME}"
 
-    RESULT=$(curl --fail -sS \
+    echo -n "Creating ${GHORG}/${NAME} "
+
+    RESULT=$(curl -sS \
         -u "${GHUSER}:${GHTOKEN}" \
         -X POST \
         -H "Accept: application/vnd.github.v3+json" \
         "https://api.github.com/orgs/${GHORG}/repos" \
         -d "{\"name\":\"${NAME}\",\"has_issues\":false,\"has_projects\":false,\"has_wiki\":false}"
     )
+
+    MESSAGE=$(echo "$RESULT" | jq -r '.errors[0].message')
+
+    if [[ "$MESSAGE" == "name already exists on this account" ]]; then
+        echo "skipped"
+        continue;
+    fi
+
+    if [[ "$MESSAGE" != "null" ]]; then
+        echo "$MESSAGE"
+        exit 1
+    fi
+
+    echo "OK"
 done
